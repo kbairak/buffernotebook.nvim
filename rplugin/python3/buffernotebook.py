ad1513b717f8897bc9ad99b2ca694c256ccbb216
Konstantinos Bairaktaris <kbairak@transifex.com>
2025-02-27 20:42:30 +0200

    Inject command

--- rplugin/python3/buffernotebook.py
+++ rplugin/python3/buffernotebook.py
@@ -1,6 +1,8 @@
 import ast
+import pprint
 import re
 import threading
+from typing import Any
 
 import pynvim
 import pynvim.api
@@ -9,8 +11,11 @@ import pynvim.api
 # - [✅] One command with one argument for everything
 # - [✅] Render complex assigns  (eg `a, b = 1, 2`)
 # - [✅] Command/mapping to clear cache and re-evaluate
-# - [ ] Command/mapping to save results in buffer
-# - [ ] Figure something out for multiline
+# - [✅] Command/mapping to save results in buffer
+# - [✅] Figure something out for multiline
+
+
+nothing_to_show = object()
 
 
 class BufferNotebook:
@@ -22,7 +27,7 @@ class BufferNotebook:
         self.namespace = self.nvim.api.create_namespace("BufferNotebookNamepsace")
 
         self.globals = {"__name__": "__main__"}
-        self.cache = []
+        self.cache: list[tuple[str, Any]] = []
         self.timer = None
 
     def enable(self):
@@ -56,7 +61,7 @@ class BufferNotebook:
 
         lines = self.nvim.api.buf_get_lines(self.buffer, 0, -1, False)
 
-        tree = self.parse()
+        tree = self.parse(lines)
 
         top_level_statements: list[tuple[int, int, ast.stmt]] = [
             (
@@ -68,49 +73,36 @@ class BufferNotebook:
         ]
 
         inline_marks, fullline_marks = set(), set()
-        for i, line in enumerate(lines):
+        for index, line in enumerate(lines):
             if re.search(r"#=\s*$", line):
-                inline_marks.add(i)
+                inline_marks.add(index)
             elif re.search(r"^#\s<<<\s*$", line):
-                fullline_marks.add(i)
+                fullline_marks.add(index)
 
-        for i, (start_line_number, end_line_number, statement) in enumerate(
+        for index, (start_line_number, end_line_number, statement) in enumerate(
             top_level_statements
         ):
-            key = ast.dump(statement)
-            try:
-                cache_key, cache_result = self.cache[i]
-            except IndexError:
-                result = self.run_statement(statement)
-                self.cache.append((key, result))
-            else:
-                if key == cache_key:
-                    result = cache_result
-                else:
-                    self.cache = self.cache[:i]
-                    result = self.run_statement(statement)
-                    self.cache.append((key, result))
+            result = self.evaluate_statement(index, statement)
 
             for line_number in (
                 set(range(start_line_number, end_line_number + 1)) & inline_marks
             ):
-                self.echo(line_number, result)
+                self.annotate(line_number, result)
 
             try:
-                (next_start_line_number, *_) = top_level_statements[i + 1]
+                (next_start_line_number, *_) = top_level_statements[index + 1]
             except IndexError:
                 for line_number in fullline_marks:
                     if line_number > end_line_number:
-                        self.echo(line_number, result)
+                        self.annotate(line_number, result)
             else:
                 for line_number in (
                     set(range(end_line_number + 1, next_start_line_number))
                     & fullline_marks
                 ):
-                    self.echo(line_number, result)
+                    self.annotate(line_number, result)
 
-    def parse(self) -> ast.Module:
-        lines = self.nvim.api.buf_get_lines(self.buffer, 0, -1, False)
+    def parse(self, lines: list[str]) -> ast.Module:
         return ast.parse("\n".join(self._parse(lines)))
 
     def _parse(self, lines: list[str]) -> list[str]:
@@ -142,7 +134,18 @@ class BufferNotebook:
         # Return: [""] +        [""] + _parse([G G ...])
         return [""] + self._parse(lines[1:])
 
-    def run_statement(self, statement: ast.stmt) -> str:
+    def evaluate_statement(self, index: int, statement: ast.stmt) -> Any:
+        key = ast.dump(statement)
+        try:
+            cache_key, cache_result = self.cache[index]
+        except IndexError:
+            pass
+        else:
+            if key == cache_key:
+                return cache_result
+            else:
+                self.cache = self.cache[:index]
+
         if isinstance(statement, ast.Assign):
             try:
                 exec(
@@ -154,10 +157,10 @@ class BufferNotebook:
                     self.globals,
                 )
             except Exception as exc:
-                return f"! {exc!r}"
+                result = exc
 
             if len(statement.targets) != 1:
-                return ""
+                result = nothing_to_show
             if isinstance(statement.targets[0], ast.Tuple) and all(
                 isinstance(elt, ast.Name) for elt in statement.targets[0].elts
             ):
@@ -171,11 +174,11 @@ class BufferNotebook:
                             self.globals,
                         )
                     )
-                return repr(tuple(result))
+                result = tuple(result)
             elif isinstance(statement.targets[0], ast.Name):
-                return repr(self.globals[statement.targets[0].id])
+                result = self.globals[statement.targets[0].id]
             else:
-                return ""
+                result = nothing_to_show
 
         elif isinstance(statement, ast.AugAssign) and isinstance(
             statement.target, ast.Name
@@ -190,19 +193,18 @@ class BufferNotebook:
                     self.globals,
                 )
             except Exception as exc:
-                return f"! {exc!r}"
-            return repr(self.globals[statement.target.id])
+                result = exc
+            result = self.globals[statement.target.id]
 
         elif isinstance(statement, ast.Expr):
             try:
-                return repr(
-                    eval(
-                        compile(ast.Expression(statement.value), "<string>", "eval"),
-                        self.globals,
-                    )
+                result = eval(
+                    compile(ast.Expression(statement.value), "<string>", "eval"),
+                    self.globals,
                 )
+
             except Exception as exc:
-                return f"! {exc!r}"
+                result = exc
 
         else:
             try:
@@ -214,11 +216,21 @@ class BufferNotebook:
                     ),
                     self.globals,
                 )
-            except Exception:
-                pass
-            return ""
+            except Exception as exc:
+                result = exc
+            result = nothing_to_show
 
-    def echo(self, line_number: int, text: str):
+        self.cache.append((key, result))
+        return result
+
+    def annotate(self, line_number: int, value: Any):
+        if value == nothing_to_show:
+            return
+        elif isinstance(value, Exception):
+            text = f"! {value!r}"
+        else:
+            text = repr(value)
+
         self.nvim.api.buf_set_virtual_text(
             self.buffer, self.namespace, line_number, [(text, "Info")], {}
         )
@@ -227,6 +239,48 @@ class BufferNotebook:
         self.globals = {"__name__": "__main__"}
         self.cache = []
         self._on_change()
+
+    def inject(self):
+        if not self.enabled:
+            self.enable()
+        lines = self.nvim.api.buf_get_lines(self.buffer, 0, -1, False)
+        current_line_position = self.nvim.api.win_get_cursor(0)[0] - 1
+
+        for index, statement in enumerate(self.parse(lines).body):
+            result = self.evaluate_statement(index, statement)
+            if (
+                statement.lineno - 1
+                <= current_line_position
+                <= (statement.end_lineno or statement.lineno)
+            ):
+                if result == nothing_to_show:
+                    break
+
+                inject_at = statement.end_lineno or statement.lineno
+
+                if isinstance(result, Exception):
+                    chunks = [f"# <<< ! {result!r}"]
+                if isinstance(result, str):
+                    pprinted_lines = result.splitlines()
+                    chunks = [f"# <<< {pprinted_lines[0]}"] + [
+                        f"# ... {pprinted_line}" for pprinted_line in pprinted_lines[1:]
+                    ]
+                else:
+                    pprinted_lines = pprint.pformat(
+                        result, sort_dicts=False, underscore_numbers=True
+                    ).splitlines()
+                    chunks = [f"# <<< {pprinted_lines[0]}"] + [
+                        f"# ... {pprinted_line}" for pprinted_line in pprinted_lines[1:]
+                    ]
+                self.nvim.api.buf_set_lines(
+                    self.buffer,
+                    inject_at,
+                    inject_at,
+                    False,
+                    chunks,
+                )
+
+                break
 
 
 @pynvim.plugin
@@ -263,9 +317,11 @@ class BufferNotebookPlugin:
             self.get_notebook().toggle()
         elif subcommand == "reset":
             self.get_notebook().reset()
+        elif subcommand == "inject":
+            self.get_notebook().inject()
         else:  # pragma: no cover
             raise Exception("Unreachable code")
 
     @pynvim.function("BufferNotebookCompletions", sync=True)
     def get_completions(self, *_):
-        return ["enable", "disable", "toggle", "reset"]
+        return ["enable", "disable", "toggle", "reset", "inject"]
